f8b55978aab17a72f73e88addf658b05fdc52b14
mmeeppoo <88130453+SaSithon29@users.noreply.github.com>
2022-12-19 11:17:17 +0700

    Main

--- test/test_m3/test-paginate-SaSithon29-main/paginateManagement.js
+++ test/test_m3/test-paginate-SaSithon29-main/paginateManagement.js
@@ -7,15 +7,18 @@ import { products } from './data/products.js'
 
 //64130500004 กษิดิ์เดช พลายเผือก
 function paginateManagement(items, rows) {
+//  - **products**: assign "items" parameter to initial products
+// - **rowsPerPage**: assign "rows" parameter to set up number of rows per page
   const rowsPerPage = rows
   const products = items
 
+
   const showItemsOfCurrentPage = (currentPageNumber = 1) => {
     // firstly call getItemsOfCurrentPage(allItems, currentPage, rowsPerPage) 
-    // by sending three parameters: products, a current page number, and rows per page. 
+    // by sending three parameters: "products", a current "page number", and "rows per page". 
     const items = getItemsOfCurrentPage(products, currentPageNumber, rowsPerPage)
 
-    // must dynamically create <li> elements for each single product 
+    //Then you must dynamically create <li> elements for each single product 
     // item under <ul id="products"></ul>
     const ulProducts = document.getElementById('products')
     ulProducts.textContent = ''
@@ -26,8 +29,11 @@ function paginateManagement(items, rows) {
       ulProducts.appendChild(liElement)
     })
   }
+
+
   const pageHandler = (event) => {
     //getElementsByTagName method returns an HTMLCollection.
+        // 'when a user click on any page number button'
     const btnAll = document.getElementsByTagName('button') //AllButton
 
     Array.from(btnAll).forEach(btn => {
@@ -39,22 +45,24 @@ function paginateManagement(items, rows) {
 
     if (event !== undefined) {
       // function to show items on a user’s page number and adding 
-      // stylesheet 'background-color: LightSteelBlue' 
-      // to a current page button.
+  // stylesheet 'background-color: LightSteelBlue' to a current page button.
 
       const btnActive = event.target
       btnActive.style = 'background-color: LightSteelBlue'
       showItemsOfCurrentPage(Number(event.target.id))
     } 
     else {
-    // the first page must be shown as default page.
+      // the first page must be shown as default page.
       const btnAtiveFirst = document.getElementById(1)
       btnAtiveFirst.style = 'background-color: LightSteelBlue'
       showItemsOfCurrentPage(1)
   }
 
+
+  
   }
   const showPageNumbers = () => {
+    // call getTotalPages(allItems, rowsPerPage)_ 
     const totalPage = getTotalPages(products, rowsPerPage)
 
     // must dynamically create <button> elements for showing each of 
@@ -62,11 +70,11 @@ function paginateManagement(items, rows) {
     const ulPagination = document.querySelector('.pagination')
 
     // must assign a 'page number' "value" to  , attribute name 'id' 
-    // for each <button>, 
-    for (let i = 1; i <= totalPage; i++) {
+    // for each <button>, until `<button id="totalPage">` of the last page
+    for (let pages = 1; pages <= totalPage; pages++) {
       const btn = document.createElement('button')
-      btn.id = i
-      btn.textContent = i
+      btn.id = pages
+      btn.textContent = pages
       ulPagination.appendChild(btn)
 
       // Do not forget to "add button click hander function" to each <button> 
