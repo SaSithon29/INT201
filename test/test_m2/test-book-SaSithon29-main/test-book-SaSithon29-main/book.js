64c293839bbb13fc5ed89475252d8a3ab649a9ba
mmeeppoo <88130453+SaSithon29@users.noreply.github.com>
2023-02-16 00:37:20 +0700

    -

--- test/test_m2/test-book-SaSithon29-main/test-book-SaSithon29-main/book.js
+++ test/test_m2/test-book-SaSithon29-main/test-book-SaSithon29-main/book.js
@@ -1,8 +1,47 @@
 const { template } = require('@babel/core')
 // 64130500001 sasithon dontree
 class Book {
+    constructor(isbn, title = 'untitled', ebook = false) {
+        this.isbn = isbn
+        this.title = title
+        this.ebook = ebook
+        this.authors = []
+    }
+    addAuthor(fn, ln) {
+        //-****: creates an author object by 
+        //assigning the firstname property with the provided parameter named _fn_ and
+        //the lastname property with the provided parameter named _ln_. 
+        //Then adding it to the end of authors array. 
+        // You must return the new length of the authors array.
+        return this.authors.push({ firstname: fn, lastname: ln })
+    }
+
+    getFirstAuthor() {
+        return this.authors?.[0]
+    }
+
+    getCoAuthors(){
+        return this.authors?.slice(1)
+    }
+
+    findAuthor(firstname, lastname) {
+        return this.authors.find(author => (author.firstname.toLowerCase() === firstname.toLowerCase()) && (author.lastname.toLowerCase() === lastname.toLowerCase()))
+    }
+
+    hasEbook(){
+        return this.ebook
+    }
 
 }
+// ** : 
+// returns the first author object that matches both firstname and lastname 
+// parameters (case insensitive). If no author matches, 
+// undefined is returned.
+
+
+
+
+
 
 // เช็คด้วยการสร้างโนดและสร้างอ็อบเจคตัวเองก่อน
 module.exports = Book
